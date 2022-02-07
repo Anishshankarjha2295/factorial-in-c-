815b2c7112ada96ede211080e6c799955b930d3e
Anishshankarjha2295 <99156836+Anishshankarjha2295@users.noreply.github.com>
2022-02-07 08:46:04 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cin>> n;
+
+  int fac =1;
+  for(int i=1;i<=n;i++){
+    fac = fac * i;
+  }
+  cout<<fac<<endl;
+
+
+  return 0;
+}
